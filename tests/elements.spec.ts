f15166a24f7ddccc9966ec6dcffecb26d2250686
Alisa Kapitanova <alisa_kapitanova@ukr.net>
2025-12-24 20:24:26 +0100

    elements

---
+++ tests/elements.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import user from '../data/elements.json';
+
+test('elements: Text Box', async ({ page }) => {
+
+    const userNameInput = page.locator('#userName');
+    const email = page.locator('#userEmail');
+    const adress = page.locator('#currentAddress');
+    const output = page.locator('#output');
+    const outputName = page.locator('#name');
+    const outputEmail = page.locator('#email');
+    const outputAdress = page.locator(' #output #currentAddress');
+    
+    await page.goto('/');
+    await page.getByText('Elements').click();
+    await page.getByText('Text Box').click();
+    await userNameInput.fill(user.fullName);
+    await email.fill(user.email);
+    await adress.fill(user.adress);
+    await page.locator('#submit').click();
+
+    await expect(output).toBeVisible();
+    await expect(outputName).toContainText(user.fullName);
+    await expect(outputAdress).toContainText(user.adress);
+    await expect(outputEmail).toContainText(user.email);
+});
